b664d59813ac0e3b7c8f49671f462f54997a85fa
atulkumar <atulkumar@localhost>
2024-06-25 17:16:40 +0530

    static vaiable

---
+++ 05Project/src/mainPack/MainClass.java
@@ -0,0 +1,23 @@
+package mainPack;
+
+
+/**
+ * This class is used for  demonstrating static 
+@author atulkumar
+@version 1.0.0
+ */
+public class MainClass {
+	//x variable
+	static int x;//used for x
+	
+	/**
+	 * This is main class
+	 * @param args it will not be used here
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(MainClass.x);
+		
+	}
+
+}
