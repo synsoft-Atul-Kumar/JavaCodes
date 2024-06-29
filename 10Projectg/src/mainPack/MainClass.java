3dd649115e6c165054fd851eee4e11089c00ec17
atulkumar <atulkumar@localhost>
2024-06-29 10:44:52 +0530

    commit "to test new main method style"

--- 10Projectg/src/mainPack/MainClass.java
+++ 10Projectg/src/mainPack/MainClass.java
@@ -3,7 +3,7 @@ package mainPack;
 public class MainClass {
 
 	
-	static void Main(){
-		System.out.println("hello");
+	 public static void main(String[] args) {
+		System.out.println("hello ,");
 	}
 }
